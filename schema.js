7b64af044199ba2feeb93bbbac6e0e66289b66e3
Brian De Maio <brian_demaio@wgresorts.com>
2019-02-11 15:53:08 -0500

    Added rocket query to root for posterity

--- schema.js
+++ schema.js
@@ -57,6 +57,29 @@ const RootQuery = new GraphQLObjectType({
             return res.data;
           });
       }
+    },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parent, args) {
+        return axios.get("https://api.spacexdata.com/v3/rockets").then(res => {
+          return res.data;
+        });
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        rocket_id: {
+          type: GraphQLString
+        }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/rockets/${args.rocket_id}`)
+          .then(res => {
+            return res.data;
+          });
+      }
     }
   }
 });
