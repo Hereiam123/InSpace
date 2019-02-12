651f447ec2d691e2d60a9259b396c6b5616c8507
Brian De Maio <brian_demaio@wgresorts.com>
2019-02-12 12:03:37 -0500

    Added ApolloProvider and Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import logo from "./space_x.png";
 import "./App.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>SpaceX</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h1>
+            <img
+              src={logo}
+              alt="SpaceX"
+              style={{ width: 300, display: "block", margin: "auto" }}
+            />
+          </h1>
+        </div>
+      </ApolloProvider>
     );
   }
 }
