9a41a553e88be5ac5a89be31accb62713dcf14c9
Brian De Maio <bdemaio1@gmail.com>
2019-02-12 14:18:31 -0500

    Created mission success or failure color styling

---
+++ client/src/components/MissionKey.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MissionKey() {
+  return (
+    <div className="my-3">
+      <p>
+        <span className="px-3 mr-2 bg-success" /> = Successful <br />
+        <span className="px-3 mr-2 bg-danger" /> = Failed
+      </p>
+    </div>
+  );
+}
