a53edfb86404078f136ce4630a7b863605a49280
Brian De Maio <bdemaio1@gmail.com>
2019-02-12 15:20:45 -0500

    Displaying individual launch details on Launch component route

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -31,13 +31,54 @@ export class Launch extends Component {
           {({ loading, error, data }) => {
             if (loading) return <h4>Loading...</h4>;
             if (error) console.log(error);
+            const {
+              mission_name,
+              flight_number,
+              launch_year,
+              launch_success,
+              rocket: { rocket_id, rocket_name, rocket_type }
+            } = data.launch;
             return (
               <div>
                 <h1 className="display-4 my-3">
-                  <span className="text-dark">
-                    Mission: {data.launch.mission_name}
-                  </span>
+                  <span className="text-dark">Mission: {mission_name}</span>
                 </h1>
+                <h4 className="mb-3">Launch Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Flight Number: {flight_number}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Year: {launch_year}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Successful:{" "}
+                    <span
+                      className={classNames({
+                        "text-success": launch_success,
+                        "text-danger": !launch_success
+                      })}
+                    >
+                      {launch_success ? "Yes" : "No or Future Launch"}
+                    </span>
+                  </li>
+                </ul>
+                <h4 className="my-3">Rocket Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Flight Number: {flight_number}
+                  </li>
+                  <li className="list-group-item">
+                    Rocket Name: {rocket_name}
+                  </li>
+                  <li className="list-group-item">
+                    Rocket Type: {rocket_type}
+                  </li>
+                </ul>
+                <hr />
+                <Link to="/" className="btn btn-secondary">
+                  Go Back
+                </Link>
               </div>
             );
           }}
