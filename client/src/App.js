c59b5fe607a90b28278248d8cfe729d1c50db5f1
Brian De Maio <bdemaio1@gmail.com>
2019-02-12 14:37:59 -0500

    Added Launch component to view an individual launch's details

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./space_x.png";
 import "./App.css";
 import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
@@ -13,16 +15,19 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container">
-          <h1>
-            <img
-              src={logo}
-              alt="SpaceX"
-              style={{ width: 300, display: "block", margin: "auto" }}
-            />
-          </h1>
-          <Launches />
-        </div>
+        <Router>
+          <div className="container">
+            <h1>
+              <img
+                src={logo}
+                alt="SpaceX"
+                style={{ width: 300, display: "block", margin: "auto" }}
+              />
+            </h1>
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
