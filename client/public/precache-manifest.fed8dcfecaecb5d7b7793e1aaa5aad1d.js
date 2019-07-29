fcdb2e5324ccd4fedd0f8f995f7472be20e7a964
hereiam123 <bdemaio1@gmail.com>
2019-07-28 21:02:54 -0400

    Built the project for future deploy

---
+++ client/public/precache-manifest.fed8dcfecaecb5d7b7793e1aaa5aad1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "02973ee55fdb531176c3",
+    "url": "/static/js/main.02973ee5.chunk.js"
+  },
+  {
+    "revision": "a469320fb41a867f89d5",
+    "url": "/static/js/2.a469320f.chunk.js"
+  },
+  {
+    "revision": "02973ee55fdb531176c3",
+    "url": "/static/css/main.f3972a36.chunk.css"
+  },
+  {
+    "revision": "9a91d0cb185acbad9d4c5d7bea6cec22",
+    "url": "/index.html"
+  }
+];
